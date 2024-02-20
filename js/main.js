5e169174c4f898ba1380accb8ac2bffa6f05f02f
ezaleta <egonzalezzaleta@gmail.com>
2024-02-19 22:22:36 -0600

    Modal dialogs added, work in progress...

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 let draggableCards = document.querySelectorAll('.draggable');
 let dropArea = document.querySelectorAll(".dropArea");
 let dropRect = document.getElementById("dropRect");
-let closeModal = document.querySelector('.closeModal');
-let modal = document.querySelector('#webDevModal');
+let closeWDModal = document.querySelector('#close-WD-Modal');
+let closeBEModal = document.querySelector('#close-BE-Modal');
+let closeFEModal = document.querySelector('#close-FE-Modal');
+let modal1 = document.querySelector('#webDevModal');
+let modal2 = document.querySelector('#backEndModal');
+let modal3 = document.querySelector('#frontEndModal');
 
 let cardIsDragged = false;
-let showModal = false;
+let showModal = [false,false,false];
+let id = 0;
 
 draggableCards.forEach (card => {
     card.addEventListener('dragstart', () => {
-        card.classList.add('draggingCard')
+        card.classList.add('draggingCard');
         dropRect.classList.add('dropRect');
 
 })
     card.addEventListener('dragend', () => {
-        card.classList.remove('draggingCard')
+        card.classList.remove('draggingCard');
         dropRect.classList.remove('dropRect');
 
         if (cardIsDragged == true) {
-            card.classList.add('PostDrag');
+            id ++;
+            card.classList.add(`PostDrag-${id}`);
             card.classList.remove('PreDrag');
             cardIsDragged = false;
-            modal.showModal();
+                console.log(id);
+            switch (id) {
+                case 1:
+                    modal1.showModal();
+                    break;
+                case 2:
+                    modal2.showModal();
+                    break;
+                case 3:
+                    modal3.showModal();
+                    break;
+                default: console.log("No card");
+                    break;
+            }
+            
         }
     })
 })
@@ -35,6 +55,14 @@ dropArea.forEach (area => {
     })
 })
 
-closeModal.addEventListener("click", () => {
-    modal.close();
+closeWDModal.addEventListener("click", () => {
+    modal1.close();
+});
+
+closeBEModal.addEventListener("click", () => {
+    modal2.close();
+});
+
+closeFEModal.addEventListener("click", () => {
+    modal3.close();
 });
